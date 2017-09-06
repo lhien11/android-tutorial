1918082627a27a0355cda5e1dedaacd6f02dd5bd
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-09-06 00:41:02 -0500

    Add Instagram Project to projects

---
+++ Instagram /ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("dfdc85678606bc073fe26b5184fd8e7134a1d12d")
+            .clientKey("77981d09179b3bdcfa745dff61c74629f6beeb5e")
+            .server("http://ec2-52-14-129-87.us-east-2.compute.amazonaws.com:80/parse/")
+            .build()
+    );
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
